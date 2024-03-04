7d32af2a45974f3582bf51bf8b94f528674986bd
admin <admin@DESKTOP-ODN9BPE>
2024-03-04 14:28:26 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("aaa");
+    }
+}
+
+
